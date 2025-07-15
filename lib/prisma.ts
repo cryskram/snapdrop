9fcc9940b57d93d26bd7404e427da0ebe4699304
cryskram <vageeshgn2005@gmail.com>
2025-07-15 22:12:48 +0530

    added prisma and database

---
+++ lib/prisma.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@/app/generated/prisma";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({ log: ["error", "info", "query", "warn"] });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
